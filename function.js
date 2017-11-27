db9ce080d8c9805b77009aadd3937da4753a198c
Bijay Sharma <bijays.nitdgp@gmail.com>
2017-11-26 18:25:54 -0600

    First Commit

---
+++ function.js
@@ -0,0 +1,7 @@
+function greetUser(name,lastname){
+  return 'Hello' +' '+ name + ' '+  lastname +'!';
+}
+
+var greeting = greetUser('Bijay','Sharma');
+console.log(greeting);
+var result1 = greetUser('Vj','S');
